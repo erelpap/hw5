25c101e0ffa93739216c3c1ecb2a283c295b3ce9
Erel Papo <papo@usc.edu>
2022-12-04 07:00:33 +0000

    Initial commit for hw5

---
+++ schedwork-driver.cpp
@@ -0,0 +1,60 @@
+// main() program to test your schedule function.
+// Feel free to modify this program in any way you need for testing.
+// It will not be graded. 
+#include <set>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+#include "schedwork.h"
+
+using namespace std;
+
+void printSchedule(const DailySchedule& sched);
+
+int main()
+{
+    // Replace the matrix below with your test case
+    // AvailabilityMatrix avail = { 
+    //     {1, 1, 1, 1},
+    //     {1, 0, 1, 0},
+    //     {1, 1, 0, 1},
+    //     {1, 0, 0, 1}
+    // };
+
+     AvailabilityMatrix avail = { 
+        {1},
+		{1},
+        {1}
+    };
+    DailySchedule sched;
+    bool solutionFound = schedule(avail, 1, 3, sched);
+    // cout << sched.size() << endl;
+    // cout << sched[3].size()<< endl;
+    if(solutionFound)
+    {
+        printSchedule(sched);
+    }
+    else
+    {
+        cout << "No solution found!" << endl;
+    }
+    return 0;
+}
+
+void printSchedule(const DailySchedule& sched)
+{
+    int day = 0;
+    for(auto s : sched)
+    {
+        cout << "Day " << day << ": ";
+        for(auto nurse : s)
+        {
+            cout << nurse << " ";
+        }
+        cout << endl;
+        day++;
+    }
+}
